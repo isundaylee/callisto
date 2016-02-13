48743e2eda7bab2a934b3c5361769f56b8392d31
Mitchell Gu <me@mitchgu.com>
2016-02-13 17:45:10 -0500

    underscore classes to dashes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213214105) do
+ActiveRecord::Schema.define(version: 20160213222939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20160213214105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "active"
+  end
+
+  create_table "trainings", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "skill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
